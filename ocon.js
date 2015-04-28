13bfacc170f984b0ceda18a6a635145fe3c0b0ce
chris <chris@bumblehead.com>
2015-04-28 00:34:27 -0700

    small change

--- ocon.js
+++ ocon.js
@@ -1,27 +1,26 @@
 // Filename: ocon.js
-// Timestamp: 2015.04.27-15:08:48 (last modified)  
+// Timestamp: 2015.04.28-00:33:29 (last modified)  
 // Author(s): Bumblehead (www.bumblehead.com)
 
-var ocon = ((typeof module === 'object') ? module : {}).exports = (function (p, o) {
-
-  o = {
-    put : function (obj, composefn) {
-      composefn(obj);
-      return obj;
-    },
-    putarr : function (obj, composefnarr) {
-      return composefnarr.map(function (fn) {
-        obj = o.put(obj, fn);
-      }) && obj;
-    }
-  };
+var ocon = ((typeof module === 'object') ? module : {}).exports = (function (p) {
 
   p = function (args) {
-    return o.putarr(function (obj) {
-      return o.putarr(obj, args);
+    return p.putarr(function (obj) {
+      return p.putarr(obj, args);
     }, (args = [].slice.call(arguments, 0)));
   };
+  
+  p.put = function (obj, composefn) {
+    composefn(obj);
+    return obj;
+  };
+  
+  p.putarr = function (obj, composefnarr) {
+    return composefnarr.map(function (fn) {
+      obj = p.put(obj, fn);
+    }) && obj;
+  };
 
-  return p.proto = o && p;
+  return p;
 
 }());
